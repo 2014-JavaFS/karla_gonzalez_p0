032ea583ee619077cbadf4105a33f62b5a41b86f
Karla Gonzalez <gkarla33@gmail.com>
2024-07-03 16:44:42 -0500

    Added initial files and began planning for the start of implementation. Also added user stories to readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# karla_gonzalez_p0
+# Banking Application
+
+## Overview
+
+A banking application built for Project 0.
+
+## User Stories
+1. As a user:
+   1. I want to be able to sign up
+      1. So that I can easily access my funds
+   2. I want to be able to login to my account
+      1. So that I can view my account and make deposits and withdrawals
+   3. I want to be able to create a new account
+      1. So that I can have a place to store my money
+   4. I want to be able to deposit funds into my account
+      1. So that I can safely store my next paycheck
+   5. I want to be able to withdraw funds from my account
+      1. So that I can pay for things
+   6. I want to be able to view my account balance(s)
+      1. So that I know how much money I have to spend
---
+++ src/BankingApplication.java
@@ -0,0 +1,50 @@
+package src;
+
+public class BankingApplication {
+    /*
+    Must-Have Functionalities:
+        1. Register new user account
+            - Password
+            - Username
+            - First Name
+            - Last Name
+        2. Login
+            - Password
+            - Username
+        3. Create at least one account
+            - Account Type
+                - Checking
+                - Savings
+            - Generate account number/id
+        4. View account balances
+            - Show account type
+            - Show account balance
+        5. Deposit funds
+            - Double amount > 0
+            - Account to deposit to
+        6. Withdraw Funds
+            - Double amount > 0 && <= amount in account
+            - Account to withdraw from
+
+    Suggested Bonus User Stories:
+        1. view the transaction history for an account
+        2. create multiple accounts per user (checking, savings, etc.)
+        3. share a joint account with another user
+        4. transfer money between accounts
+
+    Minimum Features:
+        1. Basic validation of user input (e.g. no registration for classes outside of registration window, no negative deposits/withdrawals, no overdrafting, etc.)
+        2. Unit tests for all business-logic classes
+        3. All exceptions are properly caught and handled
+        4. Proper use of OOP principles
+        5. Database is 3rd Normal Form Compliant
+        6. Referential integrity (e.g. if a class is removed from the catalog, no students should be registered for it)
+        7. Logging messages and exceptions to a file using a logger
+        8. Generation of basic design documents (e.g. relational diagram, class diagram, flows, etc.)
+    */
+
+
+    public static void main(String[] args) {
+
+    }
+}
