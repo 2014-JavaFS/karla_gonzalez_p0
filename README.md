6e5bb8aae2cf72e06347053330addb56132a580a
Karla Gonzalez <gkarla33@gmail.com>
2024-07-10 16:56:32 -0500

    Added documentation to main class and updated readMe

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 A banking application built for Project 0.
 
 ## User Stories
-1. As a user:
+### As a user:
    1. I want to be able to sign up
       1. So that I can easily access my funds
-   2. I want to be able to login to my account
+   2. I want to be able to log into my account
       1. So that I can view my account and make deposits and withdrawals
    3. I want to be able to create a new account
       1. So that I can have a place to store my money
@@ -18,3 +18,38 @@ A banking application built for Project 0.
       1. So that I can pay for things
    6. I want to be able to view my account balance(s)
       1. So that I know how much money I have to spend
+
+## Must-Have Functionalities:
+   1. Register new user account
+      - Password
+      - Email
+      - First Name
+      - Last Name
+      - An auto-generated user Id
+   2. Login
+      - Password
+      - Username
+   3. Create at least one account
+      - Account Type
+      - Checking
+      - Savings
+   4. View account balances
+      - Show account type
+      - Show account balance
+   5. Deposit funds
+      - Amount of type double
+      - Amount should be greater than zero
+   6. Withdraw Funds
+      - Amount of type double
+      - Amount should be greater than zero
+      - Amount should be less than the current account balance
+
+## Minimum Features:
+   1. Basic validation of user input (e.g. no registration for classes outside of registration window, no negative deposits/withdrawals, no overdrafting, etc.)
+   2. Unit tests for all business-logic classes
+   3. All exceptions are properly caught and handled
+   4. Proper use of OOP principles
+   5. Database is 3rd Normal Form Compliant
+   6. Referential integrity (e.g. if a class is removed from the catalog, no students should be registered for it)
+   7. Logging messages and exceptions to a file using a logger
+   8. Generation of basic design documents (e.g. relational diagram, class diagram, flows, etc.)
--- src/main/java/com/revature/bankingapp/BankingApplication.java
+++ src/main/java/com/revature/bankingapp/BankingApplication.java
@@ -13,48 +13,6 @@ import com.revature.bankingapp.util.auth.AuthService;
 import java.util.Scanner;
 
 public class BankingApplication {
-    /*
-    Must-Have Functionalities:
-        1. Register new user account
-            - Password
-            - Email
-            - First Name
-            - Last Name
-        2. Login
-            - Password
-            - Username
-        3. Create at least one account
-            - Account Type
-                - Checking
-                - Savings
-            - Generate account number/id
-        4. View account balances
-            - Show account type
-            - Show account balance
-        5. Deposit funds
-            - Double amount > 0
-            - Account to deposit to
-        6. Withdraw Funds
-            - Double amount > 0 && <= amount in account
-            - Account to withdraw from
-
-    Suggested Bonus User Stories:
-        1. view the transaction history for an account
-        2. create multiple accounts per user (checking, savings, etc.)
-        3. share a joint account with another user
-        4. transfer money between accounts
-
-    Minimum Features:
-        1. Basic validation of user input (e.g. no registration for classes outside of registration window, no negative deposits/withdrawals, no overdrafting, etc.)
-        2. Unit tests for all business-logic classes
-        3. All exceptions are properly caught and handled
-        4. Proper use of OOP principles
-        5. Database is 3rd Normal Form Compliant
-        6. Referential integrity (e.g. if a class is removed from the catalog, no students should be registered for it)
-        7. Logging messages and exceptions to a file using a logger
-        8. Generation of basic design documents (e.g. relational diagram, class diagram, flows, etc.)
-    */
-
     /**
      * An input validation method which ensures the user enters a numerical value wherever an integer is required.
      * If the user input anything that isn't an integer, an error message is displayed
@@ -123,8 +81,16 @@ public class BankingApplication {
         } while (opt != 3);
     }
 
+    /**
+     * Uses the user's id to retrieve their account from the database.
+     * If no account is found, the user is prompted to create one before being able to perform any other actions
+     *
+     * @param scanner           for user input
+     * @param user              the user currently using this application
+     * @param userController    class needed for the different account functionalities
+     */
     private static void accessAccount(Scanner scanner, User user, UserController userController) {
-        int opt;
+        int opt = 0;
         AccountController accountController = new AccountController(scanner, new AccountService());
         Account account = accountController.getAccountById(user.getUserId());
 
@@ -140,38 +106,41 @@ public class BankingApplication {
             System.out.println("3. Make a Withdrawal");
             System.out.println("4. Log Out");
             System.out.print("Please enter a numeric choice: ");
-            opt = scanner.nextInt();
 
-            System.out.println();
+            if (anyInt.isValid(scanner, "\nInvalid input type. Please enter a number 1-3")) {
+                opt = scanner.nextInt();
 
-            switch (opt) {
-                case 1:
-                    System.out.println("Viewing Account Information");
-                    userController.viewUserInfo(user);
-                    accountController.viewBalance(account);
-                    break;
+                System.out.println();
 
-                case 2:
-                    System.out.println("Making a Deposit");
-                    accountController.deposit(account);
-                    break;
-
-                case 3:
-                    if(account.getAccountBalance() <= 0) {
-                        System.out.println("Insufficient funds. Unable to make a withdrawal at this time");
+                switch (opt) {
+                    case 1:
+                        System.out.println("Viewing Account Information");
+                        userController.viewUserInfo(user);
+                        accountController.viewBalance(account);
                         break;
-                    }
 
-                    System.out.println("Making a Withdrawal");
-                    accountController.withdraw(account);
-                    break;
+                    case 2:
+                        System.out.println("Making a Deposit");
+                        accountController.deposit(account);
+                        break;
 
-                case 4:
-                    System.out.println("Exiting Application.");
-                    break;
+                    case 3:
+                        if (account.getAccountBalance() <= 0) {
+                            System.out.println("Insufficient funds. Unable to make a withdrawal at this time");
+                            break;
+                        }
 
-                default:
-                    System.out.println("Invalid input. Please enter a number 1-4");
+                        System.out.println("Making a Withdrawal");
+                        accountController.withdraw(account);
+                        break;
+
+                    case 4:
+                        System.out.println("Exiting Application.");
+                        break;
+
+                    default:
+                        System.out.println("Invalid input. Please enter a number 1-4");
+                }
             }
         } while (opt != 4);
     }
